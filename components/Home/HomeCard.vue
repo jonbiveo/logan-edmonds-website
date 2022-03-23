d7371c73722e50154091ccc30d0977edc0481ec7
jonbiveo <logantedmonds@gmail.com>
2022-03-22 19:14:40 -0500

    Added new component for home page

---
+++ components/Home/HomeCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          
+        </v-card>
+      </v-col>
+      <v-col>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HomeCard'
+}
+</script>
