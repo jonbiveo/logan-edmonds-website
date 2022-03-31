6b24b2d02ed8adb0ffe0a1a78ba1ad799ecbb47a
jonbiveo <logantedmonds@gmail.com>
2022-03-31 11:55:36 -0500

    removed icon comments

--- pages/index.vue
+++ pages/index.vue
@@ -11,14 +11,3 @@ export default {
   }
 }
 </script>
-
-// <v-icon>mdi-language-javascript</v-icon>
-// <v-icon>mdi-language-java</v-icon>
-// <v-icon>mdi-vuejs</v-icon>
-// <v-icon>mdi-react</v-icon>
-// <v-icon>mdi-nuxt</v-icon>
-// <v-icon>mdi-nodejs</v-icon>
-// <v-icon>mdi-language-html5</v-icon>
-// <v-icon>mdi-language-css3</v-icon>
-// <v-icon>mdi-bootstrap</v-icon>
-// <v-icon>mdi-vuetify</v-icon>
