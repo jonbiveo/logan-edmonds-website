88ffcf23b29661d9ace5eb625d2874b1ca4aebc3
jonbiveo <logantedmonds@gmail.com>
2022-03-31 11:27:58 -0500

    Fixed side bar color and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -58,23 +58,24 @@
     app
     expand-on-hover
     permanent
+    color="sidebar"
     >
-    <v-list>
-      <v-list-item-group>
-        <v-list-item
-        v-for="nav in sideNav"
-        :key="nav.name"
-        :to="nav.link"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="nav.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="nav.name"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item
+          v-for="nav in sideNav"
+          :key="nav.name"
+          :to="nav.link"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="nav.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="nav.name"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
     <v-main id="main">
       <v-container id="container">
@@ -82,8 +83,10 @@
       </v-container>
     </v-main>
     <v-footer
-      :absolute="fixed"
       app
+      inset
+      class="justify-center pl-0"
+      color="footer"
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
--- vuetify.options.js
+++ vuetify.options.js
@@ -5,12 +5,15 @@ export default {
       dark: {
         primary: '#001254',
         accent: '#001254',
-        secondary: '#FFFFFFF',
+        secondary: '#FFFFFF',
         info: '#001254',
         warning: '#001254',
         error: '#001254',
         success: '#001254',
-        background: '#001254'
+        background: '#001254',
+        sidebar: '#030913',
+        footer: '#1D1D1E',
+        bgtest: '#1D1D1E'
       },
       light: {
         primary: '#052490',
